db050b4b63ef1d8154cd5ec3d642785fdc23bfb3
krazyKoder <sab.sultana@gmail.com>
2021-07-13 22:03:54 -0700

    <ta4J>

---
+++ src/main/java/com/tow/core/symbols/Technical_Analysis_ta4j_API.java
@@ -0,0 +1,11 @@
+package com.tow.core.symbols;
+
+public class Technical_Analysis_ta4j_API {
+
+	// Data Aggregation : https://github.com/sstrickx/yahoofinance-api
+	// Technical Analysis: https://github.com/ta4j/ta4j
+	// https://github.com/ta4j/ta4j/blob/master/ta4j-examples/src/main/java/ta4jexamples/Quickstart.java
+
+	// TODO methods 
+	
+}
